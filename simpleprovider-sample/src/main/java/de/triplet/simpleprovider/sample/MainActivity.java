1cae48d6d4e60786fb4e0d3d6c30ea2e0fd12833
Bjoern Hurling <bjoern.hurling@wissenswerft.net>
2013-11-15 17:23:12 +0100

    Query the ContentProvider and dump the cursor

--- simpleprovider-sample/src/main/java/de/triplet/simpleprovider/sample/MainActivity.java
+++ simpleprovider-sample/src/main/java/de/triplet/simpleprovider/sample/MainActivity.java
@@ -1,7 +1,41 @@
 package de.triplet.simpleprovider.sample;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.net.Uri;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = Uri.parse("content://" + getString(R.string.authority) + "/posts");
+        return new CursorLoader(this, uri, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        DatabaseUtils.dumpCursor(data);
+
+        // ... do whatever you want with the cursor
+
+        data.close();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 
 }
