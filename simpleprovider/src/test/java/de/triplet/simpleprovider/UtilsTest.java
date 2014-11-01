f1990232338efe112d65efc69477f938d81e4b97
Christian Becker <christian.becker@qnips.com>
2014-11-01 21:17:15 +0100

    Updated SDK, Gradle wrapper, tools versions

--- simpleprovider/src/test/java/de/triplet/simpleprovider/UtilsTest.java
+++ simpleprovider/src/test/java/de/triplet/simpleprovider/UtilsTest.java
@@ -2,7 +2,7 @@ package de.triplet.simpleprovider;
 
 import org.junit.Test;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class UtilsTest {
 
@@ -10,31 +10,31 @@ public class UtilsTest {
     public void pluralize() {
         String expected = "whiskies";
         String actual = Utils.pluralize("Whisky");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
 
         expected = "ytitties";
         actual = Utils.pluralize("YTitty");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
 
         expected = "ways";
         actual = Utils.pluralize("Way");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
 
         expected = "boys";
         actual = Utils.pluralize("Boy");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
 
         expected = "journeys";
         actual = Utils.pluralize("Journey");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
 
         expected = "beers";
         actual = Utils.pluralize("Beer");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
 
         expected = "ballantines";
         actual = Utils.pluralize("Ballantines");
-        assertThat(actual).isEqualTo(expected);
+        assertEquals(expected, actual);
     }
 
 }
